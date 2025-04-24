93ef87cb65ad713c84962461cd32456d7bb56e2f
livi5599 <obrown1@uwyo.edu>
2025-04-24 17:11:44 -0600

    Update code.js

--- code.js
+++ code.js
@@ -21,7 +21,7 @@ function quicksort(array) {
         if (isSorted(array)) {
             return array;
         }
-        for (i = 0; i < array.length - 1; i++) {
+        for (let i = array.length - 1; i >= 0; i--) {
             if (array[i] == pivot && i == pivIndex) {
                 continue;
             }
