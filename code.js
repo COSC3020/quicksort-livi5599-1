1b3fc852322739a881c76c032866847204bc894d
livi5599 <obrown1@uwyo.edu>
2025-04-24 16:21:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -18,34 +18,49 @@ function quicksort(array) {
         high = stack.pop();
         low = stack.pop();
         let pivot = placePivot(array, low, high);
+        console.log("array after placePivot = ", array);
         if (isSorted(array)) {
             return array;
         }
         for (i = 0; i < array.length - 1; i++) {
+            // if (isSorted(array)) {
+            //     return array;
+            // }
             if (array[i] == pivot) {
+                console.log(array[i], " = pivot, continuing...");
                 continue;
             }
             else if (array[i] > pivot && i < array.indexOf(pivot)) {
+                console.log(array[i], "> pivot and ", i, "< pivot index");
                 array.splice(array.indexOf(pivot) + 1, 0, array[i]);
+                console.log("array after 1st splice = ", array);
                 array.splice(i, 1);
+                console.log("array after 2nd splice = ", array);
             }
             else if (array[i] < pivot && i > array.indexOf(pivot)) {
+                console.log(array[i], "< pivot and ", i, "> pivot index");
                 array.unshift(array[i]);
+                console.log("array after unshift = ", array);
                 array.splice(i+1, 1);
+                console.log("array after splice = ", array);
             }
         }
         if (isSorted(array)) {
             return array;
         }
 
-        if (array.indexOf(pivot) > 0 ) {
+        if (array.indexOf(pivot) > 0) {
+            console.log("elements to the left of pivot");
             stack.push(0);
             stack.push(array.indexOf(pivot) - 1);
+            console.log("updated stack = ", stack);
         }
 
         if (array.indexOf(pivot) < array.length - 1 && array.indexOf(pivot) + 1 < array.length - 1) {
+            console.log("elements to the right of pivot");
             stack.push(array.indexOf(pivot) + 1);
             stack.push(array.length - 1);
+            console.log("updated stack = ", stack);
         }
     }
     return array;
@@ -60,9 +75,10 @@ function placePivot(a, low, high) {
     else {
         mid = Math.floor((high - low) / 2);
     }
+
     if (mid == null) {
         pivArr.push(a[low], a[high]);
-        pivot = high;
+        pivot = a[high];
         a[low] = pivArr[0];
         a[high] = pivot;
     }
@@ -78,6 +94,7 @@ function placePivot(a, low, high) {
         a[low] = pivArr[0];
         a[mid] = pivot;
         a[high] = pivArr[2];
+        console.log("a after placePivot = ", a);
         
     }
 
@@ -94,3 +111,6 @@ function isSorted(a) {
     }
     return sorted;
 }
+
+let arr = [3, 1, 2, 4];
+console.log(quicksort(arr));
