f7f9dcc0813ea7972d13be56771e6c797ec3ca14
livi5599 <obrown1@uwyo.edu>
2025-05-07 17:51:25 -0600

    Update code.js

--- code.js
+++ code.js
@@ -33,71 +33,18 @@ function quicksort(array) {
 }
 
 function partition(a, low, high) {
-    //let i = low - 1; //used to determine which elements are <= pivot
-    //pick the pivot
-    let pivot = null;
-    let pivArr = [];
-    if ((high - low) + 1 <= 2) {
-        var mid = null;
-        console.log("mid set to null");
-    }
-    else {
-        mid = Math.floor((high - low) / 2);
-        console.log("mid = ", mid);
-    }
+    let mid = low + Math.floor((high - low) / 2);
+    let pivArr = [a[low], a[mid], a[high]].sort((x, y) => x - y);
+    let pivot = pivArr[1];
 
-    if (mid == null) {
-        pivArr.push(a[low], a[high]);
-        if (!isSorted(pivArr)) {
-            pivArr.sort();
-        }
-        console.log("pivArr = ", pivArr);
-        pivot = a[high];
-        console.log("pivot = ", pivot);
-    }
-    else {
-        pivArr.push(a[low], a[mid], a[high]);
-        console.log("pivArr = ", pivArr);
-
-        if (!isSorted(pivArr)) {
-            pivArr.sort();
-        }
-        console.log("pivArr = ", pivArr);
-
-        pivot = pivArr[1];
-        //let pivIndex = a.indexOf(pivot);
-        console.log("pivot = ", pivot);
-        //console.log("pivIndex = ", pivIndex);
-    }
-    
-    //let pivIndex = a.indexOf(pivot, low);
-    let i = low-1;
-    let j = high+1;
+    let i = low - 1;
+    let j = high + 1;
     while (true) {
-        do {
-            ++i;
-        } while (a[i] < pivot);
-
-        do {
-            --j;
-        } while (a[j] > pivot);
-
-        if (i >= j) {
-            return j;
-        }
-
+        do {++i;} while (a[i] < pivot);
+        do {--j;} while (a[j] > pivot);
+        if (i >= j) return j;
         [a[i], a[j]] = [a[j], a[i]];
     }
-    // for (let j = low; j <= high; j++) {
-    //     if (a[j] < pivot && j > pivIndex) {
-    //         console.log(a[j], " is less than pivot but is to the right of pivot");
-
-    //     }
-    // }
-
-    // [a[i+1], a[pivIndex]] = [a[pivIndex], a[i+1]];
-    // console.log("a after pivot placement = ", a);
-    // return i+1;
 }
 
 function isSorted(a) {
